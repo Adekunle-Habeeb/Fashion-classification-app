d46c83bba320c7e61c905b12faa7baacc3dd503c
Adekunle-Habeeb <adekunle22traiwo@gmail.com>
2024-09-11 10:57:30 +0100

    feat

--- api/helper.py
+++ api/helper.py
@@ -9,15 +9,12 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="Fashion Classifier", page_icon="👚", layout="wide")
 
 # Loading the Keras model
-# @st.cache_resource
+@st.cache_resource
 def load_model():
-    return tf.keras.models.load_model("/saved_model/1.keras")
-try:
-    MODEL = tf.keras.models.load_model("/saved_model/1.keras")
-except ValueError as e:
-    print(f"Error loading model: {e}")
+    return tf.keras.models.load_model("../saved_model/1.keras")
 
-    
+MODEL = load_model()
+
 # Class names 
 CLASS_NAMES = ['Boys-Apparel', 'Boys-Footwear', 'Girls-Apparel', 'Girls-Footwear']
 
